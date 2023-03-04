70faed6fa489309a3982bf39929d628f1ccff580
Joelmora19 <108998021+Joelmora19@users.noreply.github.com>
2023-03-03 22:31:58 -0600

    Proyecto

---
+++ src/test/java/com/LaHuellita/LahuellitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaHuellita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LahuellitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
